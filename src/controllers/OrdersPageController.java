6cd0eb668cfaf54734f2765e80020e8119e0a259
study-alien <edenalaluf80@gmail.com>
2026-07-02 20:22:21 +0300

    page

--- src/controllers/OrdersPageController.java
+++ src/controllers/OrdersPageController.java
@@ -1,14 +1,27 @@
 package controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import Utils.ConsolePrinter;
 import application.Main;
+import homework2.Customer;
+import homework2.DeliveryDataBase;
 import homework2.Order;
+import homework2.Restaurant;
+import homework2.Services;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
 
 public class OrdersPageController {
+	
+	private ObservableList<Order> orders = FXCollections.observableArrayList(Main.DDB.getOrders());
 	
 	@FXML
 	private TableColumn<Order, Integer> customerCol;
@@ -38,15 +51,75 @@ public class OrdersPageController {
     private TableView<Order> tblAllOrders;
     
     @FXML
+    private TextField text2;
+    
+    @FXML
     void showOrders(ActionEvent event) {
-    	System.out.println("no good");
+    	tblAllOrders.setItems(orders);
+
+    }
+    
+    @FXML
+    void showCustomerOrders(ActionEvent event) {
+    	try {
+    		int code = Integer.parseInt(text2.getText());
+    		Customer customer = Services.findCustomer(code, Main.DDB.getCustomers());
+    		if (customer != null) {
+    			ArrayList<Order> customerOrders = Main.DDB.getCustomerOrders().get(customer.getCustomerCode());
+            	tblAllOrders.setItems(FXCollections.observableArrayList(customerOrders));
+    		}
+	    } catch (Exception e) {
+			ConsolePrinter.printError(e);
+		}
     }
     
     @FXML
     void showHighestPriceOrder(ActionEvent event) {
-    	
+    	try {
+    		Order order = orders.stream().max(Order.getComparator()).orElse(null);
+        	if (order != null) {
+        		tblAllOrders.setItems(FXCollections.observableArrayList(List.of(order)));
+        	}
+	    } catch (Exception e) {
+			ConsolePrinter.printError(e);
+		}
     }
     
+    @FXML
+    private TextField text3;
+    
+    @FXML
+    void showOrdersByRest(ActionEvent event) {
+	    try {
+			int code = Integer.parseInt(text3.getText());
+			Restaurant restaurant = Services.findRestaurant(code, Main.DDB.getRestaurants());
+			if (restaurant != null) {
+				ArrayList<Order> restOrders = Main.DDB.getCustomerOrders().get(restaurant.getRestaurantCode());
+	        	tblAllOrders.setItems(FXCollections.observableArrayList(restOrders));
+			}
+	    } catch (Exception e) {
+			ConsolePrinter.printError(e);
+		}
+    }
+    
+   
+    
+    @FXML
+    private TextField text1;
+
+    @FXML
+    void findOrder(ActionEvent event) {
+    	try {
+    		int code = Integer.parseInt(text1.getText());
+        	Order order = Services.findOrder(code, Main.DDB.getOrders());
+        	tblAllOrders.setItems(FXCollections.observableArrayList(List.of(order)));
+	    } catch (Exception e) {
+			ConsolePrinter.printError(e);
+		}
+    	
+
+    }
+
     @FXML
     public void initialize() {
     	customerCol.setCellValueFactory(new PropertyValueFactory<>("customerCode"));
@@ -57,7 +130,7 @@ public class OrdersPageController {
     	priceCol.setCellValueFactory(new PropertyValueFactory<>("finalPrice"));
     	statusCol.setCellValueFactory(new PropertyValueFactory<>("deliveryStatus"));
     	resturantCol.setCellValueFactory(new PropertyValueFactory<>("restaurantCode"));
-    	System.out.println(Main.DDB.getOrders());
-    	tblAllOrders.getItems().addAll(Main.DDB.getOrders());
+    	tblAllOrders.setItems(orders);
+    	
     }
 }
